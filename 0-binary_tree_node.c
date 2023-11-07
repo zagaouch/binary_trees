fe1ca5aeca174ae44224003e01ec23282f8da8db
zagaouch <i.zagaouch@gmail.com>
2023-11-07 16:36:20 +0100

    task1

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -9,13 +9,11 @@
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 binary_tree_t *node = malloc(sizeof(binary_tree_t));
-
-	if (node)
-{
+if (node == NULL)
+return (NULL);
 		node->parent = parent;
 		node->left = NULL;
 		node->right = NULL;
 		node->n = value;
-	}
 	return (node);
 }
