ee07975023db22b392da870c7d0a2506b964cea8
zagaouch <i.zagaouch@gmail.com>
2023-11-08 17:50:27 +0100

    task6

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -4,7 +4,8 @@
  * @node : the node for check
  * Return: 1,0
 */
-int binary_tree_is_root(const binary_tree_t *node){
+int binary_tree_is_root(const binary_tree_t *node)
+{
 if (node == NULL)
 return (0);
 else
