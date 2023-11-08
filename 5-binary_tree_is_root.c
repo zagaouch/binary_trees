8d61424e70e9a8116c3d14194b8487049cd1d23a
zagaouch <i.zagaouch@gmail.com>
2023-11-08 17:49:35 +0100

    task6

---
+++ 5-binary_tree_is_root.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_is_root - check if the node is a ROOT
+ * @node : the node for check
+ * Return: 1,0
+*/
+int binary_tree_is_root(const binary_tree_t *node){
+if (node == NULL)
+return (0);
+else
+{
+if (node->parent == NULL)
+return (1);
+else
+return (0);
+}
+}
